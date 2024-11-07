103ac82e09a200e2841e0a63e4748cefe0727b93
muiddd <muidabdulmuid99@gmail.com>
2024-11-07 11:23:59 +0700

    final commit percobaan 4

---
+++ Siakad01.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Siakad01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int mahasiswa, mataKuliah;
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        mahasiswa = sc.nextInt();
+        System.out.print("Masukkan jumlah mata kuliah: ");
+        mataKuliah = sc.nextInt();
+        
+        int nilai[][] = new int[mahasiswa][mataKuliah];
+
+        for (int i = 0; i<nilai.length; i++){
+            System.out.println("Input nilai mahasiswa ke-" + (i+1));
+            double totalPerSiswa = 0;
+
+            for (int j = 0; j<nilai[i].length; j++){
+                System.out.print("Nilai mata kuliah " + (j+1) + ": ");
+                nilai[i][j] = sc.nextInt();
+                totalPerSiswa += nilai[i][j];
+            }
+            System.out.println("Nilai rata-rata: " + totalPerSiswa/mataKuliah);
+        }
+
+        System.out.println("\n================================");
+        System.out.println("Rata-rata Nilai setiap Mata Kuliah: ");
+
+        for (int j=0; j < mataKuliah; j++){
+            double totalPerMatkul = 0;
+
+            for (int i=0; i < mahasiswa; i++){
+                totalPerMatkul += nilai[i][j];
+            }
+            System.out.println("Mata Kuliah "+ (j + 1)+ ": "+ totalPerMatkul/mahasiswa);
+        }
+    }
+}
