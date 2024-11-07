9acd8030a3ece72587c2a0a6c04c153377a0e493
muiddd <muidabdulmuid99@gmail.com>
2024-11-07 07:57:08 +0700

    final commit percobaan 1

--- Bioskop01.java
+++ Bioskop01.java
@@ -20,10 +20,9 @@ public class Bioskop01 {
         for (String[] barisPenonton: penonton){
             System.out.println("Panjang baris: " + barisPenonton.length);
         }
-
-        System.out.println("Penonton pada baris ke-3: ");
-        for (int i = 0; i<penonton[2].length; i++){
-            System.out.println(penonton[2][i]);
+        
+        for (int i = 0;i < penonton.length;i++){
+            System.out.println("Penonton pada baris ke-" + (i+1) + ": " + String.join(", ", penonton[i]));
         }
     }
 }
